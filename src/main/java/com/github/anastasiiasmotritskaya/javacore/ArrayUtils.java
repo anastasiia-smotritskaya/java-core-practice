4927a7b0443507c7d0a12b8ca7059ec5aa10ca9d
anastasiia-smotritskaya <122874063+anastasiia-smotritskaya@users.noreply.github.com>
2025-12-19 22:06:14 +0300

    Feat/arrays (#12)
    * feat: add ArrayUtils class, ArrayUtilsTest class and findMinMax method with tests and javadoc
    
    * feat: add bubbleSort method with tests and javadoc
    
    * feat: add findInMatrix method with tests and javadoc
    
    * feat: add transposeMatrix method with tests and javadoc
    
    * feat: add mergeSortedArrays method with tests and javadoc

--- src/main/java/com/github/anastasiiasmotritskaya/javacore/ArrayUtils.java
+++ src/main/java/com/github/anastasiiasmotritskaya/javacore/ArrayUtils.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 /**
  * Работа с одномерными и двумерными массивами
- * Методы: поиск максимума и минимума
+ * Методы: поиск максимума и минимума, пузырьковая сортировка, поиска элемента в двумерном массиве, транспонирование матрицы
  *
  * @author Анастасия Смотрицкая
  * @version 1.0
@@ -80,7 +80,7 @@ public class ArrayUtils {
      * @param matrix двумерный массив целых чисел
      * @param target элемент для поиска
      * @return массив с координатами [row, col] или [-1, -1] если не найден
-     * {@code int[] targetCoordinate = findInMatrix(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}}, 4)} || [1, 0]
+     * {@code int[] targetCoordinate = findInMatrix(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}}, 4)}); || [1, 0]
      * @throws IllegalArgumentException если matrix null или пустой
      */
     public static int[] findInMatrix(int[][] matrix, int target) {
@@ -101,5 +101,102 @@ public class ArrayUtils {
             }
         }
         return new int[]{-1, -1};
+    }
+
+    /**
+     * Транспонирует матрицу (меняет строки и столбцы местами)
+     *
+     * @param matrix исходная матрица
+     * @return транспонированная матрица
+     * {@code int[][] transposedMatrix = transposeMatrix(new int[][]{{1, 2, 3}, {4, 5, 6});}} || {{1, 4}, {2, 5}, {3, 6}}
+     * @throws IllegalArgumentException если матрица не прямоугольная или null
+     */
+    public static int[][] transposeMatrix(int[][] matrix) {
+        if (matrix == null) {
+            throw new IllegalArgumentException("Matrix cannot be null.");
+        }
+
+        if (matrix.length == 0) {
+            throw new IllegalArgumentException("Matrix cannot be empty (0 rows).");
+        }
+
+        for (int[] row : matrix) {
+            if (row == null || row.length == 0) {
+                throw new IllegalArgumentException("The matrix cannot have empty rows.");
+            }
+        }
+
+        for (int i = 0; i < matrix.length - 1; i++) {
+            if (matrix[i].length != matrix[i + 1].length) {
+                throw new IllegalArgumentException("The matrix must be rectangular.");
+            }
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] transposedMatrix = new int[cols][rows];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int number = matrix[i][j];
+                transposedMatrix[j][i] = number;
+            }
+        }
+
+        return transposedMatrix;
+    }
+
+    /**
+     * Объединяет два отсортированных массива в один отсортированный
+     *
+     * @param arr1 первый отсортированный массив
+     * @param arr2 второй отсортированный массив
+     * @return новый отсортированный массив, содержащий все элементы
+     * {@code int[] resultArr = mergeSortedArrays(new int[]{1, 2, 3}, new int[]{4, 5, 6})}; // {1, 2, 3, 4, 5, 6}
+     * @throws IllegalArgumentException если массивы не отсортированы, пустые или null
+     */
+    public static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
+        if (arr1 == null || arr2 == null) {
+            throw new IllegalArgumentException("Array cannot be null.");
+        }
+
+        if (arr1.length == 0 || arr2.length == 0) {
+            throw new IllegalArgumentException("Array cannot be empty.");
+        }
+
+        boolean areSorted = true;
+
+        for (int i = 0; i < arr1.length - 1; i++) {
+            if (arr1[i] > arr1[i + 1]) {
+                areSorted = false;
+            }
+        }
+
+        for (int i = 0; i < arr2.length - 1; i++) {
+            if (arr2[i] > arr2[i + 1]) {
+                areSorted = false;
+            }
+        }
+
+        if (!areSorted) {
+            throw new IllegalArgumentException("At least one array is unsorted.");
+        }
+
+        int[] result = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] <= arr2[j]) {
+                result[k++] = arr1[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+        }
+
+        while (i < arr1.length) result[k++] = arr1[i++];
+        while (j < arr2.length) result[k++] = arr2[j++];
+
+        return result;
     }
 }
