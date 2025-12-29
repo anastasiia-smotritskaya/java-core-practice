ab8db9fbe2a4fd65f9485a2d17d154d4dcfa1d52
Anastasiia Smotritskaya <anastasiia.smotritskaya@gmail.com>
2025-12-29 13:41:16 +0300

    fix: countLines_br tests - Created a test resources folder - Changed paths from absolute to relative so that they can be launched from another device

--- src/test/java/com/github/anastasiiasmotritskaya/javacore/datatest/TextFileAnalyzerTest.java
+++ src/test/java/com/github/anastasiiasmotritskaya/javacore/datatest/TextFileAnalyzerTest.java
@@ -7,15 +7,19 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TextFileAnalyzerTest {
-    final String THREE_LINES_FILE = "src\\test-files\\three-line-file.txt";
-    final String EMPTY_FILE = "src\\test-files\\empty-file.txt";
-    final String EMPTY_BEGINNING_FILE = "src\\test-files\\empty-beginning-file.txt";
-    final String EMPTY_MIDDLE_FILE = "src\\test-files\\empty-middle-file.txt";
-    final String EMPTY_END_FILE = "src\\test-files\\empty-end-file.txt";
+    final String THREE_LINES_FILE = "src/test/resources/three-lines-file.txt";
+    final String EMPTY_FILE = "src/test/resources/empty-file.txt";
+    final String EMPTY_BEGINNING_FILE = "src/test/resources/empty-beginning-file.txt";
+    final String EMPTY_MIDDLE_FILE = "src/test/resources/empty-middle-file.txt";
+    final String EMPTY_END_FILE = "src/test/resources/empty-end-file.txt";
 
     @Test
     @DisplayName("countLines should count lines in file with text")
@@ -53,11 +57,29 @@ public class TextFileAnalyzerTest {
         assertThrows(IOException.class, () -> TextFileAnalyzer.countLines_br("C:\\Users\\not-exist-file.txt"));
     }
 
-
     @ParameterizedTest
     @NullAndEmptySource
     @DisplayName("countLines should throw IllegalArgumentException when the file path is null or empty")
     void countLinesTest_IllegalArgumentException(String filePath) {
         assertThrows(IllegalArgumentException.class, () -> TextFileAnalyzer.countLines_br(filePath));
+    }
+
+    @Test
+    void testPortability() throws IOException, URISyntaxException {
+        // Вариант 1: ClassLoader
+        URL resource = getClass().getClassLoader().getResource("three-lines-file.txt");
+        System.out.println("Resource URL: " + resource);
+
+        // Вариант 2: Абсолютный путь через проект
+        String projectDir = System.getProperty("user.dir");
+        Path manualPath = Paths.get(projectDir, "src", "test", "resources", "three-lines-file.txt");
+        System.out.println("Manual path: " + manualPath);
+
+        // Оба должны работать
+        int result1 = TextFileAnalyzer.countLines_br(Paths.get(resource.toURI()).toString());
+        int result2 = TextFileAnalyzer.countLines_br(manualPath.toString());
+
+        assertEquals(3, result1);
+        assertEquals(3, result2);
     }
 }
